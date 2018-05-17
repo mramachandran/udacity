035ec90efdac866bf067ded977c9a0a632fa9b51
Manoj Ramachandran <manoj.ramach@gmail.com>
2018-05-17 18:31:18 -0400

    uploading pixel art maker

---
+++ frontend/pixel-art-maker/designs.js
@@ -0,0 +1,13 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+  // Your code goes here!
+  let grid = document.createElement('grid');
+  let grid = document.createElement('grid');
+  let tr = grid.appendChild(document.createElement('tr'));
+     let cl  = tr.appendChild(document.createElement('td'));
+  return grid;
+}
