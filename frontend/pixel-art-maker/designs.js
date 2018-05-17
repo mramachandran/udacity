51ae41e6facf1d9bc96852d0c02cecda93454e31
Manoj Ramachandran <manoj.ramach@gmail.com>
2018-05-17 18:34:56 -0400

    added eventlisteners

--- frontend/pixel-art-maker/designs.js
+++ frontend/pixel-art-maker/designs.js
@@ -5,9 +5,42 @@
 
 function makeGrid() {
   // Your code goes here!
-  let grid = document.createElement('grid');
-  let grid = document.createElement('grid');
-  let tr = grid.appendChild(document.createElement('tr'));
-     let cl  = tr.appendChild(document.createElement('td'));
-  return grid;
+  event.preventDefault(); 
+  let grid = document.getElementById('pixelCanvas');
+  let gridHeight = document.getElementById('inputHeight').value
+  let gridWidth  = document.getElementById("inputWidth").value;
+  
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  
+  for (i = 0;i<gridHeight;i++) {
+    let tr = document.createElement('tr');
+
+    tr.addEventListener('click',function(event){
+        
+       //tr.style.backgroundColor = selectedColor;
+     }
+    );
+
+    for (j = 0;j<gridWidth;j++) {
+      let cl  = document.createElement('td');
+      cl.addEventListener('click',function(event){
+        
+        let selectedColor = document.getElementById("colorPicker").value;
+        cl.style.backgroundColor = selectedColor;
+      }
+     );
+
+      tr.appendChild(cl);
+    }
+    grid.appendChild(tr);
+
+  }
+  document.body.appendChild(grid);
 }
+
+
+ 
+
+
